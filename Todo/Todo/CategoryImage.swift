527f10086845bcde78ab5f7d906cf0d53c4a76ac
zukaishi <ishizuka215@gmail.com>
2021-03-09 06:42:02 +0900

    update

---
+++ Todo/Todo/CategoryImage.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryImage.swift
+//  Todo
+//
+//  Created by 石塚裕規 on 2021/03/08.
+//
+
+import SwiftUI
+
+struct CategoryImage: View {
+    var body: some View {
+        Image(systemName: "tortoise.fill")
+    }
+}
+
+struct CategoryImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryImage()
+    }
+}
