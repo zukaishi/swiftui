5e7866f4b633a857715af04e653a2e0cd3f0d1d4
zukaishi <ishizuka215@gmail.com>
2021-03-18 07:09:28 +0900

    update

---
+++ Todo/Todo/QuickNewTask.swift
@@ -0,0 +1,42 @@
+//
+//  QuickNewTask.swift
+//  Todo
+//
+//  Created by 石塚裕規 on 2021/03/18.
+//
+
+import SwiftUI
+
+struct QuickNewTask: View {
+    let category: TodoEntity.Category
+    @State var newTask: String = ""
+
+    fileprivate func addNewTask() {
+        self.newTask = ""
+    }
+    fileprivate func cancelTask() {
+        self.newTask = ""
+    }
+    var body: some View {
+        HStack {
+            TextField("新しいタスク", text: $newTask)
+            
+            Button(action: {
+                self.addNewTask()
+            }) {
+                Text("追加")
+            }
+            Button(action: {
+                self.cancelTask()
+            }) {
+                Text("Cancel").foregroundColor(.red)
+            }
+        }
+    }
+}
+
+struct QuickNewTask_Previews: PreviewProvider {
+    static var previews: some View {
+        QuickNewTask(category: .ImpUrg_1st)
+    }
+}
